91bdf9b69b49510a59c2f9f9b3fbae987e5a9823
naohisa0820 <nao8.20y.peace@gmail.com>
2024-01-14 11:03:45 +0900

    初回

---
+++ todo/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <p>遷移先ページ一覧</p>
+      <p>
+        <Link href='/todo'>Todo</Link>
+      </p>
+    </main>
+  );
+}
---
+++ todo/src/app/todo/page.tsx
@@ -0,0 +1,7 @@
+export default function Todo() {
+  return (
+    <main>
+      <div>Todo遷移先ページ</div>
+    </main>
+  );
+}
