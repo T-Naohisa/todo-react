e6f054e9b120edbc0b519ef6b458b507eb7fa4fd
T-Naohisa <nao8.20y.peace@gmail.com>
2024-11-22 17:52:27 +0900

    イベント追加の関数の追加

--- todo/src/app/page.tsx
+++ todo/src/app/page.tsx
@@ -2,12 +2,35 @@
 import React, { useEffect, useState } from 'react';
 import { CalendaerPage } from 'components/calender';
 import { Modal } from 'components/modal';
+type Events = {
+  id: number;
+  title: string;
+  description: string;
+  start: string;
+  end: string;
+  backgroundColor: string;
+  editable: boolean;
+};
+
 export default function Home() {
   const [nowTime, setNowTime] = useState<string>('');
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [eventList, setEventsList] = useState<Events[]>([]);
+
   useEffect(() => {
     getTime();
     setTime();
+    setEventsList([
+      {
+        id: 1,
+        title: 'test',
+        description: 'test',
+        start: '2024-11-23',
+        end: '2024-11-23',
+        backgroundColor: 'green',
+        editable: true,
+      },
+    ]);
   }, []);
 
   /**
@@ -49,6 +72,18 @@ export default function Home() {
     setIsOpen(false);
     console.log('登録されたタイトル:' + title);
     console.log('登録された詳細:' + disribe);
+
+    eventList.push({
+      id: 2,
+      title: 'test2',
+      description: 'test2',
+      start: '2024-11-24',
+      end: '2024-11-24',
+      backgroundColor: 'red',
+      editable: true,
+    });
+    const updateEventList = eventList;
+    setEventsList(updateEventList);
   };
   const close = () => {
     setIsOpen(false);
@@ -58,7 +93,7 @@ export default function Home() {
     <>
       <p className="timestamp text-6xl">{nowTime}</p>
       <div className="m-5">
-        <CalendaerPage />
+        <CalendaerPage event={eventList} url="/" />
         <button
           onClick={() => {
             open();
--- todo/src/components/calender.tsx
+++ todo/src/components/calender.tsx
@@ -3,8 +3,24 @@ import React from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
-export const CalendaerPage = () => {
-  // const dateClick = (arg: DateClickArg) => {
+import { EventSourceInput, DateSelectArg } from '@fullcalendar/core/index.js';
+
+type Props = {
+  event: Events[];
+  url: string;
+};
+type Events = {
+  id: number;
+  title: string;
+  description: string;
+  start: string;
+  end: string;
+  backgroundColor: string;
+  editable: boolean;
+};
+
+export const CalendaerPage = (props: Props) => {
+  console.log('eventupdate render:' + props.event);
   const dateClick = (arg: DateClickArg) => {
     console.log('date click');
     console.log(arg);
@@ -12,14 +28,39 @@ export const CalendaerPage = () => {
     const month = date.slice(5, 7);
     const days = date.slice(8, 10);
     console.log(month, days);
+    console.log(props.event[0]?.id);
+    console.log(props.event[1]?.id);
   };
-  /**
-   * 今日の日付を取得
-   * 今月の1日目
-   * 今月の最終日を取得
-   *
-   *
-   *
-   */
-  return <FullCalendar plugins={[dayGridPlugin, interactionPlugin]} dateClick={dateClick} initialView="dayGridMonth" />;
+
+  const handleDateSelect = (selectInfo: DateSelectArg) => {
+    //テキスト入力が可能なダイアログを表示する
+    const title = prompt('Please enter a new title for your event');
+    const calendarApi = selectInfo.view.calendar;
+
+    calendarApi.unselect(); // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: '2',
+        title: 'test3',
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay,
+      });
+    }
+  };
+
+  return (
+    <>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin]}
+        dateClick={dateClick} // 日付をクリックしたときに発火する
+        initialView="dayGridMonth"
+        events={props.event as EventSourceInput}
+        selectable={true} //クリックしてドラックができるようにする
+        editable={true} //カレンダー上のイベントを変更できるようにする
+        select={handleDateSelect} //日付や時刻が選択されたときに発火する
+      />
+    </>
+  );
 };
